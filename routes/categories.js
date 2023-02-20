65c1f1e5bdf698278f36c23416885c6de5111147
Frainer Encarnación <fraineralex2001@gmail.com>
2023-02-19 23:38:26 -0400

    [FIX] fix an issue to find the categories controller in the categories route file

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const categoriesController = require('../controllers/categoriesController');
+const categoriesController = require('../controllers/CategoriesController');
 
 
 router.get("/admin-categories", categoriesController.GetAdminCategories);
